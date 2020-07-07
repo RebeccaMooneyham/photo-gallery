d67cfe244c0d6e75aa42eea01e30a7c507ffe91b
Rebecca <mooneyhamrebecca@gmail.com>
2020-07-07 14:59:18 -0600

    Working on js function

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
 function search_photos() {
   let input = document.getElementById('searchbar').value
   input = input.toLowerCase();
-  console.log(input);
+  let x = document.getElementsByClassName('text');
+  
+  
+  for (i = 0; i < x.length; i++) {  
+    if (!x[i].innerHTML.toLowerCase().includes(input)) { 
+        x[i].style.display="none"; 
+    } 
+    else { 
+        x[i].style.display="block";                  
+    } 
+} 
 }
